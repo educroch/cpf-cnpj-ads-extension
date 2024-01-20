b14769769281ae81083d540a1647e6bb5452b3fd
Eduardo <edu.c.roch@gmail.com>
2024-01-20 13:01:28 -0300

    feat: adicionado limpeza de cnpj

--- src/cpf-cnpj-helper.ts
+++ src/cpf-cnpj-helper.ts
@@ -1,16 +1,28 @@
-import { cpf } from 'cpf-cnpj-validator';
+import { cpf, cnpj } from 'cpf-cnpj-validator';
 const TAMANHO_CPF: number = 11;
+const TAMANHO_CNPJ: number = 14;
 
 export function limparFormatacao(documento: string) : string {
-    const documentoSemFormatacao = documento.replace(/\./g, '').replace(/\-/g, '');
+    const documentoSemFormatacao = documento.replace(/\./g, '').replace(/\-/g, '').replace(/\//g, '');
     
-    if (documentoSemFormatacao.length !== TAMANHO_CPF) {
-        throw new Error('Tamanho incorreto para um CPF.');
+    if (verificarSeTamanhoDoDocumentoEstaInvalido(documentoSemFormatacao)) {
+        throw new Error('Tamanho inválido para um documento.');
     }
 
-    if (!cpf.isValid(documentoSemFormatacao)) {
-        throw new Error('Número de CPF inválido.');
+    if (documentoSemFormatacao.length === TAMANHO_CPF) {
+        if (!cpf.isValid(documentoSemFormatacao)) {
+            throw new Error('Número de CPF inválido.');
+        }
+    } else {
+        if (!cnpj.isValid(documentoSemFormatacao)) {
+            throw new Error('Número de CNPJ está inválido.');
+        }
     }
-    
+
     return documentoSemFormatacao;
 }
+
+function verificarSeTamanhoDoDocumentoEstaInvalido(documentoSemFormatacao: string) {
+    return !(documentoSemFormatacao.length === TAMANHO_CPF || documentoSemFormatacao.length === TAMANHO_CNPJ);
+}
+
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,11 +1,10 @@
 import * as assert from 'assert';
 
-
 import * as vscode from 'vscode';
 import * as helper from '../../cpf-cnpj-helper';
 
-suite('Teste de limpeza de formatação.', () => {
-	vscode.window.showInformationMessage('Iniciando todos os testes.');
+suite('Teste de limpeza de formatação para CPF.', () => {
+	vscode.window.showInformationMessage('Iniciando todos os testes de limpeza de formatação do CPF.');
 
 	test('Deve retornar CPF sem formatação, quanto CPF for válido.', () => {
 		const cpf = '868.659.450-69';
@@ -17,13 +16,53 @@ suite('Teste de limpeza de formatação.', () => {
 		const cpf = '1000.659.450-69';
 		assert.throws(() => {
 			helper.limparFormatacao(cpf);
-		}, Error);
+		}, (err) => {
+			assert(err instanceof Error);
+			assert.strictEqual(err.message, 'Tamanho inválido para um documento.');
+			return true;
+		});
 	});
 
 	test('Deve retornar Error, quanto CPF for inválido.', () => {
 		const cpf = '123.456.789-12';
 		assert.throws(() => {
 			helper.limparFormatacao(cpf);
-		}, Error);
+		}, (err) => {
+			assert(err instanceof Error);
+			assert.strictEqual(err.message, 'Número de CPF inválido.');
+			return true;
+		});
+	});
+});
+
+suite('Teste de limpeza de formatação para CNPJ.', () => {
+	vscode.window.showInformationMessage('Iniciando todos os testesd de limpeza de formatação do CNPJ.');
+
+	test('Deve retornar CNPJ sem formatação, quanto CNPJ for válido.', () => {
+		const cnpj = '58.182.141/0001-52';
+		const cnpjSemFormatacao = helper.limparFormatacao(cnpj);
+		assert.strictEqual(cnpjSemFormatacao, '58182141000152');
+	});
+
+	test('Deve retornar Error, quanto tamanho do CNPJ for inválido.', () => {
+		const cnpj = '158.182.141/0001-52';
+		assert.throws(() => {
+			helper.limparFormatacao(cnpj);
+		}, (err) => {
+			assert(err instanceof Error);
+			assert.strictEqual(err.message, 'Tamanho inválido para um documento.');
+			return true;
+		});
+	});
+
+	test('Deve retornar Error, quanto CNPJ for inválido.', () => {
+		const cnpj = '12.123.456/0000-12';
+		assert.throws(() => {
+			helper.limparFormatacao(cnpj);
+		},(err) => {
+			assert(err instanceof Error);
+			assert.strictEqual(err.message, 'Número de CNPJ está inválido.');
+			return true;
+		});
 	});
 });
